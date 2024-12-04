0d4e527a756a43c843e4615cd65dbe96aeaa5e16
AbdallahIsmaili <aismaili691@gmail.com>
2024-12-04 21:42:03 +0000

    start

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Anvy!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
